11a6f10ea08c65e1687162c17c6b8a0e6f1de3a5
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2026-01-15 16:18:06 -0300

    :bug: fix BlockRightClickMixin not being an abstract class

--- src/main/java/com/heyyczer/createprotected/mixin/BlockRightClickMixin.java
+++ src/main/java/com/heyyczer/createprotected/mixin/BlockRightClickMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FTBChunks.class)
-public class BlockRightClickMixin {
+public abstract class BlockRightClickMixin {
 
     // Allow click in Numismatics Bank Terminal
     @Inject(
