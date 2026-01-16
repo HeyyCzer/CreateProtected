868a583304d9190be467073cff0753ae4636c851
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2026-01-16 19:36:45 -0300

    :art: simplified logic allowing adding blocks easier

--- src/main/java/com/heyyczer/createprotected/mixin/BlockRightClickMixin.java
+++ src/main/java/com/heyyczer/createprotected/mixin/BlockRightClickMixin.java
@@ -8,12 +8,20 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(FTBChunks.class)
 public abstract class BlockRightClickMixin {
+
+    @Unique
+    private static final List<String> ALLOWED_BLOCKS = List.of(
+            "numismatics:bank_terminal"
+    );
 
     // Allow click in Numismatics Bank Terminal
     @Inject(
@@ -24,7 +32,7 @@ public abstract class BlockRightClickMixin {
     private void onRightClick(Player player, InteractionHand hand, BlockPos pos, Direction face, CallbackInfoReturnable<EventResult> cir) {
         if (player instanceof ServerPlayer sp) {
             var blockHolder = sp.level().getBlockState(pos).getBlockHolder();
-            if (blockHolder != null && blockHolder.getRegisteredName().equals("numismatics:bank_terminal")) {
+            if (blockHolder != null && ALLOWED_BLOCKS.contains(blockHolder.getRegisteredName())) {
                 cir.setReturnValue(EventResult.pass());
             }
         }
