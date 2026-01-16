0be9fa06decca6365d0ebab1d8c7ab679acfc879
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2026-01-16 19:38:23 -0300

    :fire: remove unused onServerStarting method

--- src/main/java/com/heyyczer/createprotected/CreateProtected.java
+++ src/main/java/com/heyyczer/createprotected/CreateProtected.java
@@ -1,22 +1,17 @@
 package com.heyyczer.createprotected;
 
-import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreateProtected.MODID)
 public class CreateProtected {
+
     // Define mod id in a common place for everything to reference
     public static final String MODID = "create_protected";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -28,13 +23,6 @@ public class CreateProtected {
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
 }
